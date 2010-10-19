9127e623fa1b5554ae9ed2fdf511f8689aa08b0f
Tibor <tibor.mozes+dev@gmail.com>
2010-10-19 12:26:37 -0400

    Intial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Rubu on Rails Tutorial
+
+This is a demo app using the wonderful [*Ruby on Rails Tutorial*](http://railstutorial.org/) by [Michale Hartle]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo2App::Application.config.secret_token = '2ba8ffee25e2c2a704c66bed8999cd56c321e15d47f955dc8bcee5c591dba458cad3f4f1d45c3803a4602cc104a9e66a2db13564a50ee1ae560f5a26bf280a00'
